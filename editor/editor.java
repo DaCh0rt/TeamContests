ae272157c98affdf85dd69bdd233d82cf3198379
DaCh0rt <ianharvey95@gmail.com>
2019-03-19 13:36:08 -0400

    added comments to tc2 code

--- editor/editor.java
+++ editor/editor.java
@@ -14,6 +14,7 @@ public class editor{
 			//parse input
 			int f = in.nextInt();
 
+			//fill line length array and find max, init visited array
 			int[] ll = new int[f+1];
 			int max = 0;
 			for(int j = 1; j <= f; j++){
@@ -23,6 +24,7 @@ public class editor{
 			}
 			visited = new boolean[max+1][f+1];
 
+			//read start and end
 			int y = in.nextInt();
 			int x = in.nextInt();
 			Point start = new Point(x,y);
@@ -31,15 +33,20 @@ public class editor{
 			x = in.nextInt();
 			Point end = new Point(x,y);
 
+			//init bfs queue
 			ArrayDeque<Point> q = new ArrayDeque<Point>();
 			q.offer(start);
 
+			//run bfs until we have our answer
 			int iter = 0;
 			boolean found = false;
 			while(!q.isEmpty()){
 
+				//run thru current layer
 				int size = q.size();
 				for(int j = 0; j < size; j++){
+
+					//take next and check if it is the answer
 					Point p = q.poll();
 					if(p.x == end.x && p.y == end.y){
 						System.out.println(iter);
@@ -47,18 +54,22 @@ public class editor{
 						break;
 					}
 
+					//if already visited skip, otherwise mark visited
 					if(visited[p.x][p.y])
 						continue;
 					else
 						visited[p.x][p.y]=true;
 
+					//find next state positions
 					ArrayList<Point> toEnque = nextMoves(p,ll);
 
+					//enque all next states
 					for(int k = 0; k < toEnque.size(); k++){
 						q.offer(toEnque.get(k));
 					}
 				}
 
+				//break again out of the while when answer found
 				if(found)
 					break;
 
@@ -88,19 +99,19 @@ public class editor{
 		}
 
 		//up
-		if(cur.y-1 > 0){
-			if(cur.x <= ll[cur.y-1]){
+		if(cur.y-1 > 0){//can go up
+			if(cur.x <= ll[cur.y-1]){//pure up
 				moves.add(new Point(cur.x,cur.y-1));
-			} else{
+			} else{//up causes jump
 				moves.add(new Point(ll[cur.y-1],cur.y-1));
 			}
 		}
 
 		//down
-		if(cur.y+1 < ll.length){
-			if(cur.x <= ll[cur.y+1]){
+		if(cur.y+1 < ll.length){//can go down
+			if(cur.x <= ll[cur.y+1]){//pure down
 				moves.add(new Point(cur.x,cur.y+1));
-			} else{
+			} else{//down causes jump
 				moves.add(new Point(ll[cur.y+1],cur.y+1));
 			}
 		}
@@ -109,7 +120,7 @@ public class editor{
 	}
 }
 
-class Point{
+class Point{//just store and x and y for a point
 	public int x;
 	public int y;
 
--- mine/mine.java
+++ mine/mine.java
@@ -4,9 +4,12 @@ class mine{
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 
+		//read in rows and cols for the test case, testcases end with "0 0"
 		int r = in.nextInt();
 		int c = in.nextInt();
 		while(r!=0){
+
+			//init and read in board
 			char[][] board = new char[r][c];
 			for(int i = 0; i < r; i++){
 				String s = in.next();
@@ -17,14 +20,18 @@ class mine{
 				// System.out.println();
 			}
 
+			//go thru each spot in board
 			int[] dx = {-1,-1,-1, 0, 0, 1, 1, 1}, dy = {1, 0, -1, 1, -1, 1, 0 , -1};
 			for(int i = 0; i < r; i++){
 				for(int j = 0; j < c; j++){
+
+					//mine, skip
 					if(board[i][j] == '*'){
 						System.out.print(board[i][j]);
 						continue;
 					}
 
+					//not mine, count the mines around it
 					int val = 0;
 					for(int k = 0; k < dx.length; k++){
 						int pi = i + dx[k];
@@ -33,12 +40,15 @@ class mine{
 							if(board[pi][pj] == '*')
 								val++;	
 					}
+
+					//replace board spot with num of mines adj.
 					board[i][j] = (char)(val + '0');
 					System.out.print(board[i][j]);
 				}
 				System.out.println();
 			}
 
+			//prep next case
 			r = in.nextInt();
 			c = in.nextInt();
 		}
